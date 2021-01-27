9fea6729f068c331fbeeaf66f1bf1fd3b9894c25
Martin Diges <missingnoglitch0@gmail.com>
2021-01-26 21:36:53 -0600

    Created Projects folder, URL paths will follow same structure as file paths

---
+++ src/Projects/SimplifiedEnglish/Introduction.js
@@ -0,0 +1,14 @@
+import 'SimplifiedEnglishPages.css';
+
+function Introduction()
+{
+    return (
+        <div>
+            <h1>
+                This will be the introductory page for the Simplified English project
+            </h1>
+        </div>
+    );
+}
+
+export default Introduction;
